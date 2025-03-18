99a9bb0ff540861fe1ed8f6c83bae2220f7dc932
Joe Blau <im@joeblau.com>
2025-03-17 18:46:19 -0700

    feat: change pure to view

--- contracts/libraries/UniswapV2Library.sol
+++ contracts/libraries/UniswapV2Library.sol
@@ -16,7 +16,7 @@ library UniswapV2Library {
     }
 
     // calculates the CREATE2 address for a pair without making any external calls
-    function pairFor(address factory, address tokenA, address tokenB) internal pure returns (address pair) {
+    function pairFor(address factory, address tokenA, address tokenB) internal view returns (address pair) {
         (address token0, address token1) = sortTokens(tokenA, tokenB);
         pair = IUniswapV2Factory(factory).getPair(token0, token1);
     }
